a66cbd20c47265b8f52da0981e0180041c353e96
Iaroslav Bulimov <iar.bulimov@gmail.com>
2026-04-19 14:08:27 +0300

    Применение Fake в Pydantic-моделях

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
+
+from tools.fakers import fake
 
 
 class FileSchema(BaseModel):
@@ -15,8 +17,9 @@ class CreateFileRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание файла.
     """
-    filename: str
-    directory: str
+    filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
+    # Директорию оставляем статичной, чтобы все тестовые файлы на сервере попадали в одну папку
+    directory: str = Field(default="tests")
     upload_file: str
 
 
